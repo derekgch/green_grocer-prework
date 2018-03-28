57125b2efe8d790dacc29481ce0408b875ad337b
derek chen <derek.g.chen@gmail.com>
2018-03-28 19:36:40 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def apply_coupons(cart, coupons)
         puts cart[data[:item]]
         cart[t_name] = cart[data[:item]]
         puts cart[data[:item]] , data[:num]
-        temp = {}
+        temp = cart[t_name]
         temp = cart[data[:item]]
         temp[:price] = data[:cost]
         temp[:count] = cart[data[:item]][:count] / data[:num]
