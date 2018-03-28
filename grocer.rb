3f990a148e67f996be79f4d8cb58f4d927ed5d1a
derek chen <derek.g.chen@gmail.com>
2018-03-28 19:54:24 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -45,6 +45,7 @@ def apply_clearance(cart)
   # code here
   cart.each do |data|
     data.each do |k,v|
+      puts v
       temp = v[:price] * 0.8
       v[:price] = temp if(v[:clearance] == true)
     end
