653eaaec167daa9d109c8b6d311550b16552fa91
derek chen <derek.g.chen@gmail.com>
2018-03-28 17:03:01 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -8,7 +8,7 @@ def consolidate_cart(cart)
       dens_cart[list.keys] = list[list.keys].merge(:count=>1)
     end
   end
-  puts dens_cart
+
   dens_cart
 end
 
@@ -23,3 +23,17 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+items = 	[
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"KALE" => {:price => 3.00, :clearance => false}},
+		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+		{"ALMONDS" => {:price => 9.00, :clearance => false}},
+		{"TEMPEH" => {:price => 3.00, :clearance => true}},
+		{"CHEESE" => {:price => 6.50, :clearance => false}},
+		{"BEER" => {:price => 13.00, :clearance => false}},
+		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+		{"BEETS" => {:price => 2.50, :clearance => false}}
+	]
+
+consolidate_cart(items)
